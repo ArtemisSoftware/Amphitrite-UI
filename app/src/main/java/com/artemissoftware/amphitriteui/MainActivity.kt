0094b63e4bac477119ad24dd53858f4819b8717c
gustavo <gustavo.isaac@gmail.com>
2022-02-26 21:28:50 +0000

    Initial commit

---
+++ app/src/main/java/com/artemissoftware/amphitriteui/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.artemissoftware.amphitriteui
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.artemissoftware.amphitriteui.ui.theme.AmphitriteUITheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AmphitriteUITheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    AmphitriteUITheme {
+        Greeting("Android")
+    }
+}

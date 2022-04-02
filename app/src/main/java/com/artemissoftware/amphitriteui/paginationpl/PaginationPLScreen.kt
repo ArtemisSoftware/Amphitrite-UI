bfa11df675197607c79d98cc4f426ff5119d1813
gustavo <gustavo.isaac@gmail.com>
2022-04-02 21:49:57 +0100

    paginationpl - different logic

--- app/src/main/java/com/artemissoftware/amphitriteui/paginationpl/PaginationPLScreen.kt
+++ app/src/main/java/com/artemissoftware/amphitriteui/paginationpl/PaginationPLScreen.kt
@@ -2,8 +2,10 @@ package com.artemissoftware.amphitriteui.paginationpl
 
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -12,10 +14,14 @@ import com.artemissoftware.amphitriteui.paginationpl.composables.PageItem
 @Composable
 fun PaginationPLScreen() {
 
+    val scrollState = rememberLazyListState()
     val viewModel = viewModel<MainViewModel>()
     val state = viewModel.state
 
+
+
     LazyColumn(
+        state = scrollState,
         modifier = Modifier.fillMaxSize()
     ) {
 
@@ -23,8 +29,11 @@ fun PaginationPLScreen() {
 
             val item = state.items[i]
 
-            if (i >= state.items.size - 1 && !state.endReached && !state.isLoading) {
-                viewModel.loadNextItems()
+
+            LaunchedEffect(scrollState){
+                if (i >= state.items.size - 1 && !state.endReached && !state.isLoading) {
+                    viewModel.loadNextItems()
+                }
             }
 
             PageItem(item = item)

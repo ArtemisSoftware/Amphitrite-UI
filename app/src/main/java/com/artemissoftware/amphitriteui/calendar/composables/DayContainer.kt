328da72493f514a873e33219a24f6d0bd277921f
gustavo <gustavo.isaac@gmail.com>
2022-04-25 17:18:27 +0100

    calendar - DayContainer

---
+++ app/src/main/java/com/artemissoftware/amphitriteui/calendar/composables/DayContainer.kt
@@ -0,0 +1,49 @@
+package com.artemissoftware.amphitriteui.calendar.composables
+
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.clearAndSetSemantics
+import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.semantics.stateDescription
+import com.artemissoftware.amphitriteui.R
+import com.artemissoftware.amphitriteui.util.CalendarConstants.CELL_SIZE
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+private fun DayContainer(
+    modifier: Modifier = Modifier,
+    selected: Boolean = false,
+    onClick: () -> Unit = { },
+    onClickEnabled: Boolean = true,
+    backgroundColor: Color = Color.Transparent,
+    onClickLabel: String? = null,
+    content: @Composable () -> Unit
+) {
+    val stateDescriptionLabel = stringResource(
+        if (selected) R.string.state_descr_selected else R.string.state_descr_not_selected
+    )
+    Surface(
+        modifier = modifier
+            .size(width = CELL_SIZE, height = CELL_SIZE)
+            .then(
+                if (onClickEnabled) {
+                    modifier.semantics {
+                        stateDescription = stateDescriptionLabel
+                    }
+                } else {
+                    modifier.clearAndSetSemantics { }
+                }
+            ),
+        onClick = onClick,
+        enabled = onClickEnabled,
+        color = backgroundColor,
+        onClickLabel = onClickLabel
+    ) {
+        content()
+    }
+}
---
+++ app/src/main/java/com/artemissoftware/amphitriteui/util/CalendarConstants.kt
@@ -0,0 +1,8 @@
+package com.artemissoftware.amphitriteui.util
+
+import androidx.compose.ui.unit.dp
+
+object CalendarConstants {
+
+    val CELL_SIZE = 48.dp
+}

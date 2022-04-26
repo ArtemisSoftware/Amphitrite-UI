0241ff223e4ae94c63fde434a43a6448116a236c
gustavo <gustavo.isaac@gmail.com>
2022-04-26 09:59:08 +0100

    calendar - Week

--- app/src/main/java/com/artemissoftware/amphitriteui/calendar/composables/Day.kt
+++ app/src/main/java/com/artemissoftware/amphitriteui/calendar/composables/Day.kt
@@ -1,6 +1,7 @@
 package com.artemissoftware.amphitriteui.calendar.composables
 
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
@@ -9,18 +10,56 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.semantics.clearAndSetSemantics
+import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.tooling.preview.Preview
 import com.artemissoftware.amphitriteui.calendar.CalendarScreen
+import com.artemissoftware.amphitriteui.calendar.models.CalendarDay
 import com.artemissoftware.amphitriteui.calendar.models.DayOfWeek
 
 @Composable
-private fun Day(name: String) {
+fun Day(name: String) {
     DayContainer {
         Text(
             modifier = Modifier.wrapContentSize(Alignment.Center),
             text = name,
             style = MaterialTheme.typography.caption.copy(Color.Black.copy(alpha = 0.6f))
         )
+    }
+}
+
+
+@Composable
+fun Day(
+    day: CalendarDay,
+//    onDayClicked: (CalendarDay) -> Unit,
+//    month: CalendarMonth,
+    modifier: Modifier = Modifier
+) {
+
+//    val enabled = day.status != DaySelectedStatus.NonClickable
+
+    DayContainer(
+        modifier = modifier.semantics {
+//            if (enabled) text = AnnotatedString("${month.name} ${day.value} ${month.year}")
+//            dayStatusProperty = day.status
+        },
+//        selected = day.status != DaySelectedStatus.NoSelected,
+//        onClick = { onDayClicked(day) },
+//        onClickEnabled = enabled,
+//        backgroundColor = day.status.color(MaterialTheme.colors),
+//        onClickLabel = stringResource(id = R.string.click_label_select)
+    ) {
+//        DayStatusContainer(status = day.status) {
+            Text(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .wrapContentSize(Alignment.Center)
+                    // Parent will handle semantics
+                    .clearAndSetSemantics {},
+                text = day.value,
+                style = MaterialTheme.typography.body1.copy(color = Color.Black)
+            )
+//        }
     }
 }
 
---
+++ app/src/main/java/com/artemissoftware/amphitriteui/calendar/composables/Week.kt
@@ -0,0 +1,61 @@
+package com.artemissoftware.amphitriteui.calendar.composables
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.artemissoftware.amphitriteui.calendar.models.CalendarDay
+import com.artemissoftware.amphitriteui.calendar.models.DayOfWeek
+import com.artemissoftware.amphitriteui.util.CalendarConstants.CELL_SIZE
+
+typealias CalendarWeek = List<CalendarDay>
+
+@Composable
+fun Week(
+    modifier: Modifier = Modifier,
+//    month: CalendarMonth,
+    week: CalendarWeek,
+//    onDayClicked: (CalendarDay) -> Unit
+) {
+    //val (leftFillColor, rightFillColor) = getLeftRightWeekColors(week, month)
+
+    Row(modifier = modifier) {
+
+        val spaceModifiers = Modifier
+            .weight(1f)
+            .heightIn(max = CELL_SIZE)
+
+//        Surface(
+//            modifier = spaceModifiers,
+//            color = leftFillColor
+//        ) {
+//            Spacer(Modifier.fillMaxHeight())
+//        }
+
+        for (day in week) {
+            Day(
+                day = day,
+//                onDayClicked,
+//                month
+            )
+        }
+
+//        Surface(
+//            modifier = spaceModifiers,
+//            color = rightFillColor
+//        ) {
+//            Spacer(Modifier.fillMaxHeight())
+//        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun WeekPreview() {
+
+    Week(week = listOf(CalendarDay("2"), CalendarDay("3")))
+}

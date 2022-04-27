0a3ca2bae8615baf4e77c9fc99958276e92fba4b
gustavo <gustavo.isaac@gmail.com>
2022-04-27 09:59:23 +0100

    calendar - DaySelected

--- app/src/main/java/com/artemissoftware/amphitriteui/calendar/CalendarViewModel.kt
+++ app/src/main/java/com/artemissoftware/amphitriteui/calendar/CalendarViewModel.kt
@@ -1,4 +1,29 @@
 package com.artemissoftware.amphitriteui.calendar
 
-class CalendarViewModel {
+import androidx.lifecycle.ViewModel
+import com.artemissoftware.amphitriteui.calendar.repository.CalendarRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class CalendarViewModel @Inject constructor(
+    private val calendarRepository: CalendarRepository
+): ViewModel() {
+
+//    val datesSelected = calendarRepository.datesSelected
+//    val datesSelected = DatesSelectedState(datesLocalDataSource.year2021)
+
+
+    val calendarYear = calendarRepository.getCalendarYear()
+
+//    fun onDaySelected(daySelected: DaySelected) {
+//        viewModelScope.launch {
+//            datesRepository.onDaySelected(daySelected)
+//        }
+//    }
+//
+//
+//    suspend fun onDaySelected(daySelected: DaySelected) = withContext(defaultDispatcher) {
+//        datesSelected.daySelected(daySelected)
+//    }
 }
---
+++ app/src/main/java/com/artemissoftware/amphitriteui/calendar/models/DaySelected.kt
@@ -0,0 +1,36 @@
+package com.artemissoftware.amphitriteui.calendar.models
+
+import com.artemissoftware.amphitriteui.util.CalendarYear
+
+data class DaySelected(
+    val day: Int,
+    val month: CalendarMonth,
+    val year: CalendarYear
+) {
+//    val calendarDay = lazy {
+//        month.getDay(day)
+//    }
+//
+//    override fun toString(): String {
+//        val month = month.name
+//            .substring(0, 3)
+//            .replaceFirstChar {
+//                if (it.isLowerCase()) it.titlecase(Locale.getDefault()) else it.toString()
+//            }
+//        return "$month $day"
+//    }
+//
+//    operator fun compareTo(other: DaySelected): Int {
+//        if (day == other.day && month == other.month) return 0
+//        if (month == other.month) return day.compareTo(other.day)
+//        return (year.indexOf(month)).compareTo(
+//            year.indexOf(other.month)
+//        )
+//    }
+//
+
+    companion object{
+        val EMPTY = DaySelected(-1, CalendarMonth("", "", 0, 0, DayOfWeek.Sunday), emptyList())
+    }
+}
+

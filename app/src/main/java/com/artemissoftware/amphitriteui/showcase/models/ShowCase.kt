2a8a5397b4b12d9de37be63b0c66b3bc0c4e41f2
gustavo <gustavo.isaac@gmail.com>
2022-05-10 14:45:14 +0100

    showcase - ShowCaseText

--- app/src/main/java/com/artemissoftware/amphitriteui/showcase/models/ShowCase.kt
+++ app/src/main/java/com/artemissoftware/amphitriteui/showcase/models/ShowCase.kt
@@ -5,9 +5,23 @@ import androidx.compose.ui.layout.LayoutCoordinates
 
 data class ShowCase(
     val index: Int,
-    val coordinate: LayoutCoordinates,
+    //val coordinate: LayoutCoordinates,
     val title: String,
     val subTitle: String,
     val titleColor: Color = Color.White,
     val subTitleColor: Color = Color.White
-)
+){
+
+    companion object{
+
+        fun getMock() = ShowCase(
+                index = 1,
+                title = "The title",
+                subTitle = "the subtitle",
+                titleColor = Color.Green,
+                subTitleColor = Color.Red
+            )
+
+    }
+
+}

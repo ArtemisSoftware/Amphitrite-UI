f105d2286a7db05c333cc61bdc58291bf634624d
gustavo <gustavo.isaac@gmail.com>
2022-06-15 10:22:37 +0100

    favorite - AnimatedFavoriteButton

---
+++ app/src/main/java/com/artemissoftware/amphitriteui/animation/favorite/composables/AnimatedFavoriteButton.kt
@@ -0,0 +1,10 @@
+package com.artemissoftware.amphitriteui.animation.favorite.composables
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview(showBackground = true)
+@Composable
+fun AnimatedFavoriteButton() {
+    FavoriteButton()
+}

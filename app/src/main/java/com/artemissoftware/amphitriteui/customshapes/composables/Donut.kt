0af424de97c08e9552a812021f46b70e70a36570
gustavo <gustavo.isaac@gmail.com>
2022-07-01 09:52:54 +0100

    donut - toppings

--- app/src/main/java/com/artemissoftware/amphitriteui/customshapes/composables/Donut.kt
+++ app/src/main/java/com/artemissoftware/amphitriteui/customshapes/composables/Donut.kt
@@ -6,8 +6,6 @@ import android.graphics.DiscretePathEffect
 import android.graphics.Paint
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -22,9 +20,7 @@ import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.graphics.drawscope.clipPath
 import androidx.compose.ui.graphics.nativeCanvas
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.core.graphics.toColorInt
-import com.artemissoftware.amphitriteui.arctext.CircularText
 import kotlin.random.Random
 
 @Composable
@@ -48,86 +44,93 @@ fun Donut() {
 
         //base
 
-        val path = Path().apply {
+        val donutBasePath = Path().apply {
             this.addOval(Rect(center = center, radius = size.width / 2.5f))
         }
 
-        drawPath(path, color = Color(0xffeb768b))
+        drawBase(path = donutBasePath)
 
-        //layer de frosting
+        //frosting
 
-        drawContext.canvas.nativeCanvas.apply {
-
-            drawCircle(
-                center.x,
-                center.y,
-                (size.width / 2.5f) - 20f,
-                android.graphics.Paint().apply {
-                    this.color = icingColor
-                    pathEffect = ComposePathEffect(
-                        CornerPathEffect(100F),
-                        DiscretePathEffect(60F, 15F)
-                    )
-                }
-            )
-        }
+        drawFrostingLayer(icingColor = icingColor)
 
         //toppings
 
+        drawToppings(path = donutBasePath, radius = size.width / 2)
+
         //middle
 
-        drawContext.canvas.nativeCanvas.apply {
-            drawCircle(
-                center.x, center.y,
-                100f,
-                Paint().apply {
-                    color = android.graphics.Color.WHITE
-                    setShadowLayer(
-                        100f, 0f, -30f, baseColor
-                    )
-                }
-            )
-        }
-
-        for(x in 1..10){
-
-            val position = Random.nextInt(-1000, 1000)
-
-            drawArc(
-                color = Color.Blue,
-                startAngle = 30f,
-                sweepAngle = 300f,
-                useCenter = false,
-                style = Stroke(width = 6f, cap = StrokeCap.Round),
-                topLeft = Offset(center.x - position ,center.y - position),
-                size = Size(size.width / 2, size.height / 2),
-            )
-        }
-
+        drawMiddle()
 
 
     }
 }
 
 
-private fun DrawScope.drawSprinkles(path: Path, radius : Float) {
+private fun DrawScope.drawBase(path: Path){
+    drawPath(path, color = Color(0xffeb768b))
+}
 
-    val angle = Random.nextInt(360)
-    val diff = Random.nextInt(-1000,1000)
+private fun DrawScope.drawFrostingLayer(icingColor: Int){
 
-    clipPath(path) {
-        drawArc(
-            color = randomColors[/*Random.nextInt(randomColors.size-1)*/0],
-            startAngle = angle.toFloat(),
-            sweepAngle = Random.nextInt(3 ,8).toFloat(),
-            useCenter = false,
-            style = Stroke(
-                Random.nextInt(5 ,10).toFloat(),
-                cap = StrokeCap.Round
-            ),
-            topLeft = Offset(center.x - diff ,center.y - diff),
-            size = Size(radius,radius),
+    drawContext.canvas.nativeCanvas.apply {
+
+        drawCircle(
+            center.x,
+            center.y,
+            (size.width / 2.5f) - 20f,
+            android.graphics.Paint().apply {
+                this.color = icingColor
+                pathEffect = ComposePathEffect(
+                    CornerPathEffect(100F),
+                    DiscretePathEffect(60F, 15F)
+                )
+            }
         )
+    }
+
+}
+
+private fun DrawScope.drawMiddle(){
+
+    val baseColor = "#eb768b".toColorInt()
+
+    drawContext.canvas.nativeCanvas.apply {
+        drawCircle(
+            center.x, center.y,
+            100f,
+            Paint().apply {
+                color = android.graphics.Color.WHITE
+                setShadowLayer(
+                    100f, 0f, -30f, baseColor
+                )
+            }
+        )
+    }
+}
+
+
+private fun DrawScope.drawToppings(path: Path, radius : Float) {
+
+    for (x in 1..200) {
+
+        val angle = Random.nextInt(360)
+        val diff = Random.nextInt(-1000, 1000)
+
+        clipPath(path) {
+            drawArc(
+                color = randomColors[Random.nextInt(randomColors.size - 1)],
+                startAngle = angle.toFloat(),
+                sweepAngle = Random.nextInt(3, 8).toFloat(),
+                useCenter = false,
+                style = Stroke(
+                    Random.nextInt(5, 10).toFloat(),
+                    cap = StrokeCap.Round
+                ),
+                topLeft = Offset(center.x - diff, center.y - diff),
+                size = Size(radius, radius),
+            )
+        }
     }
 }
 
